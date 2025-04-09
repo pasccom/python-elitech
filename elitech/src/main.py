786ffb079536cd55e2868a068cb538df88251d80
Raimar Bühmann <raimar _at_ buehmann _dot de>
2025-04-09 20:30:42 +0200

    Show version defined in metadata of package

--- elitech/src/main.py
+++ elitech/src/main.py
@@ -17,12 +17,13 @@
 
 import argparse
 from .commands import Command
+import importlib.metadata
 
 def main():
     parser = argparse.ArgumentParser(description="Console tool to interact with Elitech temperature and humidity loggers")
     parser.add_argument('-d', '--dev', '--device', action='store', default='',
                         help='The device to interact with')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + importlib.metadata.version('elitech'))
     parser.add_argument('cmds', action='extend', nargs='+',
                         help="The commands to execute. To see help on a specific command, use the 'help' command.")
     args = parser.parse_args()
